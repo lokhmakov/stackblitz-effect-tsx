2da79b1ee41ebdcae7afc7f3f07f6d5f0ded438f
lokhmakov <l.pavel.m@gmail.com>
2023-08-17 21:06:37 +0300

    2 files modified

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { Effect, pipe } from 'effect';
 
-pipe(Effect.succeed(25))
-  .pipe(Effect.tap((x) => Effect.log(x)))
-  .pipe(Effect.runFork);
+const refetchToken = () =>
+  Effect.tryPromise({
+    try: () => Promise.resolve({ token: `good` }),
+    catch: () => new Error(`Cannot `),
+  }).pipe(Effect.tap(() => Effect.log(`Refetching`)));
+
+const verifyCredentials = (params: { token: string }) =>
+  pipe(
+    Effect.succeed(params),
+    Effect.filterOrElse(({ token }) => token === 'good', refetchToken)
+  ).pipe(Effect.tap((x) => Effect.log(x)));
+
+pipe(verifyCredentials({ token: `good` }), Effect.runFork);
+
+pipe(verifyCredentials({ token: `bad` }), Effect.runFork);
